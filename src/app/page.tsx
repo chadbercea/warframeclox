ce6a955553f7d03f13f4377320949db543a1830e
Chad Bercea <chadbercea@gmail.com>
2025-12-16 23:46:12 -0700

    feat: Add temporary toast test button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { SpaceBackground } from '@/components/space-background';
 import { FloatingMenu } from '@/components/floating-menu';
 import { EarthGlobe } from '@/components/earth-globe';
 import { ScrollLock } from '@/components/scroll-lock';
+import { ToastTestButton } from '@/components/toast-test-button';
 // import { AssetDebugger } from '@/components/asset-debugger';
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <SpaceBackground />
       <EarthGlobe />
       <FloatingMenu />
+      <ToastTestButton />
       {/* <AssetDebugger /> */}
 
       <div className="fixed inset-0 flex flex-col items-center justify-center z-10">
